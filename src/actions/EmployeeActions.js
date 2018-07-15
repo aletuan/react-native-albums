53b97f7fe5510fe24af495b457aba8885b946574
andy le <anhlt1983@gmail.com>
2018-07-15 11:46:57 +1000

    fix issue with prop mistake

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,15 +3,15 @@ import {
     EMPLOYEE_CREATE,
 } from './type';
 
-export const employeeUpdate = ({ props, value }) => {
+export const employeeUpdate = ({ prop, value }) => {
     return {
         type: EMPLOYEE_UPDATE,
-        payload: { props, value },
+        payload: { prop, value },
     };
 };
 
 export const employeeCreate = ( {name, phone, shift }) => {
-    console.log(name);
+    console.log(name, phone, shift);
     return {
         type: EMPLOYEE_CREATE,
         payload: { name, phone, shift },
