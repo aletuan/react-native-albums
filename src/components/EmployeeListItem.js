355c1a200120a643235bb7294695ef5b472036b3
andy le <anhlt1983@gmail.com>
2018-07-15 18:18:22 +1000

    support navigation

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { CardSection } from './commons';
 import PropTypes from 'prop-types';
+import { Actions } from 'react-native-router-flux';
 
 class EmployeeListItem extends Component {
+
+    onRowPress() {
+        // action navigation
+        Actions.employeeCreate();
+    }
+
     render() {
         const { name } = this.props.employee;
 
         return (
-            <CardSection>
-                <Text style={styles.titleStyle}>
-                    {name}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+                <View>
+                    <CardSection>
+                        <Text style={styles.titleStyle}>
+                            {name}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
